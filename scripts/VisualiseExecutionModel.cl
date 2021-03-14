033e022fbec2f1713715a1d2cde007146bf9e756
Nitish Shingde <ngshingde@gmail.com>
2021-03-14 23:37:40 +0530

    OpenCLBasics| Add visualise_execution_model function to showcase execution model of OpenCL

---
+++ scripts/VisualiseExecutionModel.cl
@@ -0,0 +1,45 @@
+__kernel void visualiseWorkItemsInGlobalSpace(__global float *matrix) {
+    __private size_t x = get_global_id(0);
+    __private size_t y = get_global_id(1);
+    __private size_t z = get_global_id(2);
+    __private size_t dim_x = get_global_size(0);
+    __private size_t dim_y = get_global_size(1);
+    __private size_t dim_z = get_global_size(2);
+    matrix[x*dim_y + y*dim_z + z] = x*dim_y + y*dim_z + z;
+}
+
+__kernel void visualiseWorkItemsInWorkGroupSpace(__global float *matrix) {
+    __private size_t x = get_global_id(0);
+    __private size_t y = get_global_id(1);
+    __private size_t z = get_global_id(2);
+    __private size_t dim_x = get_global_size(0);
+    __private size_t dim_y = get_global_size(1);
+    __private size_t dim_z = get_global_size(2);
+
+    __private size_t lx = get_local_id(0);
+    __private size_t ly = get_local_id(1);
+    __private size_t lz = get_local_id(2);
+    __private size_t dim_lx = get_local_size(0);
+    __private size_t dim_ly = get_local_size(1);
+    __private size_t dim_lz = get_local_size(2);
+
+    matrix[x*dim_y + y*dim_z + z] = lx*dim_ly + ly*dim_lz + lz;
+}
+
+__kernel void visualiseWorkGroups(__global float *matrix) {
+    __private size_t x = get_global_id(0);
+    __private size_t y = get_global_id(1);
+    __private size_t z = get_global_id(2);
+    __private size_t dim_x = get_global_size(0);
+    __private size_t dim_y = get_global_size(1);
+    __private size_t dim_z = get_global_size(2);
+
+    __private size_t gx = get_group_id(0);
+    __private size_t gy = get_group_id(1);
+    __private size_t gz = get_group_id(2);
+    __private size_t dim_gx = get_num_groups(0);
+    __private size_t dim_gy = get_num_groups(1);
+    __private size_t dim_gz = get_num_groups(2);
+
+    matrix[x*dim_y + y*dim_z + z] = gx*dim_gy + gy*dim_gz + gz;
+}
--- src/OpenCLBasics/main.cpp
+++ src/OpenCLBasics/main.cpp
@@ -1,5 +1,6 @@
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include "../Utility.h"
+#include <tuple>
 
 void hello_world() {
     cl_int status = CL_SUCCESS;
@@ -37,8 +38,62 @@ void hello_world() {
     printf("%s\n\n", buf);
 }
 
+void visualise_execution_model() {
+    cl_int status;
+
+    cl::Context context(CL_DEVICE_TYPE_GPU, nullptr, nullptr, nullptr, &status);
+    verifyOpenCL_Status(status);
+
+    auto devices = context.getInfo<CL_CONTEXT_DEVICES>(&status);
+    verifyOpenCL_Status(status);
+    auto device = devices.front();
+
+    size_t dim_x = 16, dim_y = 4;
+    float matrix[dim_x][dim_y];
+    memset(matrix, 0, sizeof(matrix));
+    cl::Buffer matrixMem(context, CL_MEM_HOST_READ_ONLY|CL_MEM_WRITE_ONLY, sizeof(matrix), nullptr, &status);
+    verifyOpenCL_Status(status);
+
+    cl::Program program(context, readScript("VisualiseExecutionModel.cl"), false, &status);
+    verifyOpenCL_Status(status);
+    verifyOpenCL_Status(program.build("-cl-std=CL1.2"));
+
+    cl::CommandQueue commandQueue(context, device, 0 &status);
+    verifyOpenCL_Status(status);
+
+    for(auto [kernelName, message]: {
+        std::make_tuple("visualiseWorkItemsInGlobalSpace", "Visualise work-items in global space:"),
+        std::make_tuple("visualiseWorkItemsInWorkGroupSpace", "Visualise work-items in work-group space:"),
+        std::make_tuple("visualiseWorkGroups", "Visualise work groups:"),
+    }) {
+        cl::Kernel kernel(program, kernelName, &status);
+        verifyOpenCL_Status(status);
+        verifyOpenCL_Status(kernel.setArg(0, matrixMem));
+        std::vector<cl::Event> blockers(1);
+        verifyOpenCL_Status(commandQueue.enqueueNDRangeKernel(
+            kernel,
+            cl::NullRange,
+            cl::NDRange(dim_x, dim_y/*, 1*/),
+            cl::NDRange(2, 2/*, 1*/),
+            nullptr,
+            &blockers.front()
+        ));
+        verifyOpenCL_Status(commandQueue.enqueueReadBuffer(matrixMem, CL_TRUE, 0, sizeof(matrix), matrix, &blockers));
+        verifyOpenCL_Status(commandQueue.finish());
+        printf("%s\n", message);
+        for(size_t i = 0; i < dim_x; ++i) {
+            for(size_t j=0; j < dim_y; ++j) {
+                printf(" %02.0f ", matrix[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     hello_world();
+    visualise_execution_model();
 
     return 0;
 }
