a50bd46973a366827a1ac4c5b2d3390c7ff00ad0
Nitish Shingde <ngshingde@gmail.com>
2021-05-21 00:11:33 +0530

    Graph| Remove unused things like parent vector while doing bfs

--- src/Graph/Graph.cpp
+++ src/Graph/Graph.cpp
@@ -7,17 +7,14 @@ std::vector<int32_t> svp::SerialBfsStrategy::search(const std::vector<std::vecto
     std::vector<int32_t> frontier;
     frontier.emplace_back(sourceNode);
     std::vector<int32_t> distances(graph.size(), -1);
-    std::vector<int32_t> parents(graph.size(), -1);
-    parents[sourceNode] = sourceNode;
 
     for(int32_t distance = 1; !frontier.empty(); ++distance) {
         std::vector<int32_t> queue;
         // process all the unvisited neighbouring nodes
-        for(int node :frontier) {
+        for(const auto node :frontier) {
             for(const auto neighbour: graph[node]) {
                 if(distances[neighbour] == -1) {
                     distances[neighbour] = distance;
-                    parents[neighbour] = node;
                     queue.emplace_back(neighbour);
                 }
             }
