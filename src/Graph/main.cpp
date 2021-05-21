6cc69fde2f4ece9df0a3cd322e63c8fcf9af327d
Nitish Shingde <ngshingde@gmail.com>
2021-05-21 15:04:07 +0530

    Graph| Verify BFS search code

--- src/Graph/main.cpp
+++ src/Graph/main.cpp
@@ -21,16 +21,42 @@ std::vector<std::vector<int32_t>> readsAdjacencyList(const char *pFileName) {
     return adjacencyList;
 }
 
+std::vector<int32_t> readAnswer(const char *pFileName) {
+    std::ifstream input(pFileName);
+    std::vector<int32_t> distances;
+    for(int32_t distance;!input.eof();) {
+        input >> distance;
+        distances.emplace_back(distance);
+    }
+    return distances;
+}
+
 int main(int argc, char **argv) {
     auto graph = readsAdjacencyList("resources/hugetrace-00010.mtx.adj");
+    auto sourceNode = 0;
+#if not NDEBUG
+    auto check = readAnswer("resources/hugetrace-00010.mtx.ans0");
+#endif
 
     for(const auto &pStrategy: {
         std::shared_ptr<svp::BfsStrategy>(new svp::SerialBfsStrategy()),
     }) {
+        std::vector<int32_t> distance;
         SVP_START_BENCHMARKING_SESSION(pStrategy->toString().c_str(), 10) {
             SVP_PRINT_BENCHMARKING_ITERATION();
-            auto distance = pStrategy->search(graph, 0);
+            distance = pStrategy->search(graph, sourceNode);
         }
+#if not NDEBUG
+        if(check != distance) {
+            printf("[Debug] Failed!\n");
+            return -1;
+        }
+#else
+        if(distance.empty() or distance.size() < sourceNode or distance[sourceNode] != 0) {
+            printf("[Debug] Failed!\n");
+            return -1;
+        }
+#endif
     }
 
     return 0;
