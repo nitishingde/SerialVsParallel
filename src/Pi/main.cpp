130197d9bc21167b6f1ddd771f07d429f7f4756f
Nitish Shingde <ngshingde@gmail.com>
2021-06-11 19:22:10 +0530

    Pi| Merge the mpi related benchmarking with the non mpi ones

--- src/Pi/main.cpp
+++ src/Pi/main.cpp
@@ -1,18 +1,11 @@
+#include <algorithm>
 #include <cmath>
 #include "Pi.h"
-#include "../Utility.h"
 
-int main(int argc, char **argv) {
+void benchMark(const std::initializer_list<std::shared_ptr<svp::PiStrategy>> &strategies) {
     const int32_t ITERATIONS = 10;
-    SVP_START_BENCHMARKING_SESSION("Calculate Pi");
-    for(auto &pPiStrategy: {
-        std::shared_ptr<svp::PiStrategy>(new svp::SerialPiStrategy()),
-        std::shared_ptr<svp::PiStrategy>(new svp::OpenMP_PiStrategy()),
-        std::shared_ptr<svp::PiStrategy>(new svp::CacheFriendlyOpenMP_PiStrategy()),
-        std::shared_ptr<svp::PiStrategy>(new svp::AtomicBarrierOpenMP_PiStrategy()),
-        std::shared_ptr<svp::PiStrategy>(new svp::ReductionOpenMP_PiStrategy()),
-        std::shared_ptr<svp::PiStrategy>(new svp::OpenCL_PiStrategy()),
-    }) {
+
+    for(auto &pPiStrategy: strategies) {
         double pi = 0;
         SVP_START_BENCHMARKING_ITERATIONS(ITERATIONS) {
             SVP_PRINT_BENCHMARKING_ITERATION();
@@ -28,6 +21,31 @@ int main(int argc, char **argv) {
             pPiStrategy->toString().c_str()
         );
     }
+}
+
+int main(int argc, char **argv) {
+    SVP_START_BENCHMARKING_SESSION("Calculate Pi");
+    std::vector<std::string> arguments(argv, argv + argc);
+
+    if(std::find(arguments.begin(), arguments.end(), "--use-mpi") != arguments.end()) {
+        svp::MPI_GlobalLockGuard lock(argc, argv);
+        benchMark({
+            std::shared_ptr<svp::PiStrategy>(new svp::MPI_PiStrategy()),
+            std::shared_ptr<svp::PiStrategy>(new svp::HybridMpiOpenMP_PiStrategy()),
+        });
+        // the work of non root processes is completed here
+        if(!svp::isMpiRootPid()) return 0;
+    }
+
+    // only root process executes this
+    benchMark({
+        std::shared_ptr<svp::PiStrategy>(new svp::SerialPiStrategy()),
+        std::shared_ptr<svp::PiStrategy>(new svp::OpenMP_PiStrategy()),
+        std::shared_ptr<svp::PiStrategy>(new svp::CacheFriendlyOpenMP_PiStrategy()),
+        std::shared_ptr<svp::PiStrategy>(new svp::AtomicBarrierOpenMP_PiStrategy()),
+        std::shared_ptr<svp::PiStrategy>(new svp::ReductionOpenMP_PiStrategy()),
+        std::shared_ptr<svp::PiStrategy>(new svp::OpenCL_PiStrategy()),
+    });
 
     return 0;
 }
