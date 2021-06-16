2ad4fdbf7ce5e52aa3ccfad50982014ea8b25c6c
Nitish Shingde <ngshingde@gmail.com>
2021-06-16 22:44:52 +0530

    Refactor setup script to process graph better

--- resources/setup.py
+++ resources/setup.py
@@ -4,7 +4,26 @@ import tarfile
 import shutil
 
 
-def bfs(graph: list, sourceNode: int, out_f: str):
+class Graph:
+    def __init__(self, nodes: int, weighted: bool, directed: bool):
+        self.dict = [[] for _ in range(nodes)]
+        self.weighted = weighted
+        self.directed = directed
+
+    def add_edge(self, vertex1: int, vertex2: int, weight=0.0):
+        self.dict[vertex1].append((vertex2, weight))
+        if not self.directed and vertex1 != vertex2:
+            self.dict[vertex2].append((vertex1, weight))
+
+    def sort(self):
+        for edgeList in self.dict:
+            edgeList.sort()
+
+    def __len__(self):
+        return len(self.dict)
+
+
+def bfs(graph: Graph, sourceNode: int, out_f: str):
     frontier, new_frontier = [sourceNode], []
     costs = [-1 for _ in range(len(graph))]
     costs[sourceNode] = 0
@@ -13,7 +32,7 @@ def bfs(graph: list, sourceNode: int, out_f: str):
     while frontier:
         cost += 1
         for node in frontier:
-            for neighbour in graph[node]:
+            for neighbour, _ in graph.dict[node]:
                 if costs[neighbour] == -1:
                     costs[neighbour] = cost
                     new_frontier.append(neighbour)
@@ -28,9 +47,8 @@ def bfs(graph: list, sourceNode: int, out_f: str):
     return costs
 
 
-def dijkstra(graph: list, wgraph: list, source: int, out_f: str):
+def dijkstra(graph: Graph, source: int, out_f: str):
     from heapq import heappop, heappush
-    from _testcapi import FLT_MAX
 
     visited = [False] * len(graph)
     dist = [float('inf') for _ in range(len(graph))]
@@ -48,7 +66,7 @@ def dijkstra(graph: list, wgraph: list, source: int, out_f: str):
         if dist[node] < costs:
             continue
 
-        for adjacent_node, adjacent_weight in zip(graph[node], wgraph[node]):
+        for adjacent_node, adjacent_weight in graph.dict[node]:
             if visited[adjacent_node]:
                 continue
 
@@ -67,13 +85,8 @@ def dijkstra(graph: list, wgraph: list, source: int, out_f: str):
     return dist, prev
 
 
-def to_adjacency_matrix(graph: list, wgraph: list, out_f: str):
-    is_weighted = False
-    for adj, w in zip(graph, wgraph):
-        if 0 < len(adj):
-            if len(adj) == len(w):
-                is_weighted = True
-            break
+def to_adjacency_matrix(graph: Graph, out_f: str):
+    is_weighted = graph.weighted
 
     with open(out_f, 'w') as f:
         # total edges
@@ -85,58 +98,50 @@ def to_adjacency_matrix(graph: list, wgraph: list, out_f: str):
 
         f.write(str(len(graph)))
         f.write(os.linesep)
-        for adj_list, weight_list in zip(graph, wgraph):
+        for adj_list in graph.dict:
             f.write(str(len(adj_list)))
             f.write(os.linesep)
             if len(adj_list):
-                f.write(' '.join(map(str, adj_list)))
+                f.write(' '.join(map(str, [neighbour[0] for neighbour in adj_list])))
                 f.write(os.linesep)
-                if weight_list:
-                    f.write(' '.join(map(str, weight_list)))
+                if is_weighted:
+                    f.write(' '.join(map(str, [neighbour[1] for neighbour in adj_list])))
                     f.write(os.linesep)
 
     print('Graph saved!')
 
 
-def load_matrix_market(file_path: str, directed=False, weighted=False):
+def load_matrix_market(file_path: str):
     graph = None
-    wgraph = None
     with open(file_path, 'r') as f:
         dim = None
         i = 0
+        obj, form, field, symmetry = f.readline().strip().split(' ')[1:]
+        weighted = False if field == 'pattern' else True
+        directed = True if symmetry == 'general' else False
         for line in f:
             if line[0] == '%':
+                if 'undirected' in line:
+                    directed = False
                 continue
             elif dim is None:
                 dim = list(map(int, line.strip().split(' ')))
-                graph = [[] for _ in range(dim[0])]
-                wgraph = [[] for _ in range(dim[0])]
+                graph = Graph(max(dim[0], dim[1]), weighted=weighted, directed=directed)
             else:
                 v1, v2, w = None, None, None
-                if weighted:
-                    l = line.strip().split(' ')
-                    v2, v1 = map(int, l[:2])
-                    w = float(l[2])
-                else:
-                    v2, v1 = map(int, line.strip().split(' '))
-
+                l = line.strip().split(' ')
+                v2, v1 = map(int, l[:2])
                 v1, v2 = v1 - 1, v2 - 1
-
-                graph[v1].append(v2)
                 if weighted:
-                    wgraph[v1].append(w)
+                    w = float(l[2])
 
-                if not directed:
-                    graph[v2].append(v1)
-                    if weighted:
-                        wgraph[v2].append(w)
+                graph.add_edge(v1, v2, w)
                 i += 1
                 print(f'Loading graph: {int(i * 100 / dim[2])}%', end='\r')
 
-    # for adj_list in graph:
-    #     adj_list.sort()
+    graph.sort()
     print('Graph loaded!')
-    return graph, wgraph
+    return graph
 
 
 def load_adjacency_matrix(file_path: str):
@@ -168,35 +173,15 @@ if __name__ == '__main__':
     setup_primes()
 
     files = {
-        'appu.mtx': {
-            'directed': True,
-            'weighted': True,
-            'link': 'http://www.cise.ufl.edu/research/sparse/MM/Simon/appu.tar.gz',
-        },
-        'ca2010.mtx': {
-            'directed': False,
-            'weighted': True,
-            'link': 'https://www.cise.ufl.edu/research/sparse/MM/DIMACS10/ca2010.tar.gz',
-        },
-        'cage13.mtx': {
-            'directed': True,
-            'weighted': True,
-            'link': 'http://www.cise.ufl.edu/research/sparse/MM/vanHeukelum/cage13.tar.gz',
-        },
-        # 'wiki-Talk.mtx': {
-        #     'directed': True,
-        #     'weighted': False,
-        #     'link': '',
-        # },
-        'kron_g500-logn16.mtx': {
-            'directed': False,
-            'weighted': True,
-            'link': 'http://www.cise.ufl.edu/research/sparse/MM/DIMACS10/kron_g500-logn16.tar.gz',
-        },
+        'appu.mtx': 'http://www.cise.ufl.edu/research/sparse/MM/Simon/appu.tar.gz',
+        'ca2010.mtx': 'https://www.cise.ufl.edu/research/sparse/MM/DIMACS10/ca2010.tar.gz',
+        'cage13.mtx': 'http://www.cise.ufl.edu/research/sparse/MM/vanHeukelum/cage13.tar.gz',
+        'kron_g500-logn16.mtx': 'http://www.cise.ufl.edu/research/sparse/MM/DIMACS10/kron_g500-logn16.tar.gz',
+        'gre_512.mtx': 'https://www.cise.ufl.edu/research/sparse/MM/HB/gre_512.tar.gz',
+        'cage8.mtx': 'https://www.cise.ufl.edu/research/sparse/MM/vanHeukelum/cage8.tar.gz',
     }
-    for file, prop in files.items():
+    for file, link in files.items():
         print(f'> {file}')
-        directed, weighted, link = prop['directed'], prop['weighted'], prop['link']
         compressed_file = link.split('/')[-1]
         if not os.path.isfile(file):
             if not os.path.isfile(compressed_file):
@@ -214,10 +199,15 @@ if __name__ == '__main__':
                     shutil.rmtree(dirpath)
                     break
 
-        graph, wgraph = load_matrix_market(file, directed=directed, weighted=weighted)
-        to_adjacency_matrix(graph, wgraph, file + '.adj')
+        if os.path.isfile(file+'.adj') and os.path.isfile(file+'.ans') and os.path.isfile(file+'.ans.sssp'):
+            continue
+
+        graph = load_matrix_market(file)
+        if not os.path.isfile(file+'.adj'):
+            to_adjacency_matrix(graph, file + '.adj')
         for source_node in range(1):
-            bfs(graph, source_node, file + '.ans')
-            if weighted:
-                dijkstra(graph, wgraph, source_node, file+'.ans.sssp')
+            if not os.path.isfile(file+'.ans'):
+                bfs(graph, source_node, file + '.ans')
+            if graph.weighted and not os.path.isfile(file+'.ans.sssp'):
+                dijkstra(graph, source_node, file+'.ans.sssp')
         print()
